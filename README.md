d5cc35e951feaf6e2c550a5e2aa861458003f9c4
Augusto <augusto.robles.mza@gmail.com>
2021-03-22 18:01:10 -0300

    initial design

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a basic tic tac toe game created with HTML, CSS and vanilla JS.
+
+Design pattern: Javascript module pattern with factories functions
--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hola')
+const Player = (nickname) => {
+    let totalScore = 0;
+    return {
+        nickname,
+        totalScore
+    };
+};
+
+
+
+
+const gameboard = () => {
+    let gameBoardArr = [
+        ['X','O','X'],
+        ['O','X','O'],
+        ['O','O','X']
+        ]
+    return gameBoardArr
+}
+
+console.log(gameboard.gameBoardArr)
