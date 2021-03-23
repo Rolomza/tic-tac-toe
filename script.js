9f89f7fe9e47c714de72bbaba47fd6c5cde2c3b0
Augusto <augusto.robles.mza@gmail.com>
2021-03-23 14:23:49 -0300

    gamecontroller module

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const displayController = (() => {
 
     cellElements.forEach(cell => {
         cell.addEventListener('click', e => {
-            if (gameController.isOver() || e.target.textContent !== "") return;
+            if (gameController.getIsOver() || e.target.textContent !== "") return;
             gameController.playRound(parseInt(e.target.dataset.index));
             updateGameboard();
         })
@@ -72,5 +72,61 @@ const displayController = (() => {
 })();
 
 const gameController = (() => {
+    const playerX = Player('X');
+    const playerO = Player('O');
+    let round = 1;
+    let isOver = false;
+
+    const playRound = (cellIndex) => {
+        gameBoard.setCell(cellIndex, getCurrentPlayerSign());
+        if (checkWinner(cellIndex)) {
+            displayController.setResultMessage(getCurrentPlayerSign());
+            isOver = true;
+            return;
+        }
+        if (round === 9) {
+            displayController.setResultMessage('Draw');
+            isOver = true;
+            return;
+        }
+        round++;
+        displayController.setMessageElement(
+            `Player ${getCurrentPlayerSign()}'s turn`
+        );
+    };
+
+    const getCurrentPlayerSign = () => {
+        return round % 2 === 1? playerX.getSign() : playerO.getSign();
+    };
+
+    const checkWinner = (cellIndex) => {
+        const winConditions = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ];
+
+        return winConditions
+            .filter(combination => combination.includes(cellIndex))
+            .some(possibleCombination =>
+                possibleCombination.every(index => gameBoard.getCell(index) === getCurrentPlayerSign())
+            );
+    };
+
+    const getIsOver = () => {
+        return isOver;
+    };
+
+    const reset = () => {
+        round = 1;
+        isOver = false;
+    }
+
+    return { playRound, getIsOver, reset }
 
 })();
