58552849ccd74efa0a06a32b44a4f61a9192f05f
Augusto <augusto.robles.mza@gmail.com>
2021-03-22 23:46:29 -0300

    gameboard module

--- script.js
+++ script.js
@@ -8,13 +8,31 @@ const Player = (sign) => {
     return { getSign };
 }
 
-
 const gameboard = (() => {
+    const board = ["","","","","","","","",""];
 
+    const setCell = (index, sign) => {
+        if(index > board.length) return;
+        board[index] = sign;
+    };
+
+    const getCell = (index) => {
+        if(index > board.length) return;
+        return board[index];
+    };
+
+    const reset = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = "";
+        }
+    };
+
+    return { setCell, getCell, reset}
 })();
 
 const displayController = (() => {
-
+    const cellElements = document.querySelector('.cell');
+    const messageElement = document.querySelector('.message')
 })();
 
 const gameController = (() => {
